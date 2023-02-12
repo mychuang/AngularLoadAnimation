a49e05bd98707dc68ce69b875bc0194e8aa7ac6e
mychuang <hhjoy222@gmail.com>
2023-02-12 15:45:24 +0800

    initial home component set

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  users: any = [];
+  loading: boolean = true;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((data)=>{
+    this.users = data;
+    setTimeout(()=>{
+      this.loading = false;
+    }, 2000);
+  })
+  }
+
+}
