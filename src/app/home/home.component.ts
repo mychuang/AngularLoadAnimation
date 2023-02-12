f9474393c0e15b7dc17e235cf71ac3ba3c167fb3
mychuang <hhjoy222@gmail.com>
2023-02-12 15:56:26 +0800

    Add simple DOM

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,12 +14,12 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((data)=>{
-    this.users = data;
-    setTimeout(()=>{
-      this.loading = false;
-    }, 2000);
-  })
+    //this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((data)=>{
+    //  this.users = data;
+    //  setTimeout(()=>{
+    //    this.loading = false;
+    //  }, 2000);
+    //})
   }
 
 }
